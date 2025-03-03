3cf1a0654a4a6a59584ebc87447a537ce0e26b64
Aparna <gargaparna78@gmail.com>
2025-03-03 19:16:37 +0530

    init

--- class.c
+++ class.c
@@ -62,28 +62,51 @@
 //     return (side*side);
 // }
 
-#include <stdio.h>
-int fibonnaci(int n);
-int main(){
-    int n;
-    printf("Enter the number of terms: ");
-    scanf("%d",&n);
-    printf("Fibonnaci series: ");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ",fibonnaci(i));
-    }
-    return 0;
-}
-int fibonnaci(int n){
-    if (n<=0){
-        return 0;
-    }
-    else if (n==1){
-        return 1;
-    }
-    else{
-        return (fibonnaci(n-1)+fibonnaci(n-2));
-    }
+// #include <stdio.h>
+// int fibonnaci(int n);
+// int main(){
+//     int n;
+//     printf("Enter the number of terms: ");
+//     scanf("%d",&n);
+//     printf("Fibonnaci series: ");
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ",fibonnaci(i));
+//     }
+//     return 0;
+// }
+// int fibonnaci(int n){
+//     if (n<=0){
+//         return 0;
+//     }
+//     else if (n==1){
+//         return 1;
+//     }
+//     else{
+//         return (fibonnaci(n-1)+fibonnaci(n-2));
+//     }
 
+// }
+#include <stdio.h>
+#include<string.h>
+void reverse(char *str,int start,int end)
+{
+    if(start >= end)
+    return ;
+    else{
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        reverse(str,start+1,end-1);
+    }
+}
+int main(){
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s",str);
+   int n = strlen(str);
+    reverse(str,0,n-1);
+    printf("Reversed string: %s",str);
+
+    return 0;
 }
