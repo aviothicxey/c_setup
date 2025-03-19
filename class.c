bcf60c7aabd11e803aaac9d21a881427e46fd020
Aparna <gargaparna78@gmail.com>
2025-03-19 16:32:27 +0530

    init{

--- class.c
+++ class.c
@@ -237,3 +237,11 @@
 //     return 0;
 // }
 
+#include <stdio.h>
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int *ptr = arr;
+    printf("p\n", ptr);
+    printf("p\n", *ptr);
+    return 0;
+}
