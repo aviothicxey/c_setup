501e4f2c12e906b7fc847921834dd7b23c8f9326
Aparna <gargaparna78@gmail.com>
2025-03-19 16:29:34 +0530

    init

--- hehe.c
+++ hehe.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int num, i;
-    printf("Enter a positive integer: ");
-    scanf("%d", &num);
 
-    printf("Multiplication Table of %d:\n", num);
-    for(i = 1; i <= 10; ++i) {
-        printf("%d x %d = %d\n", num, i, num * i);
-    }
-
+    printf("Hello, World!\n");
+    
     return 0;
 }
--- practical.c
+++ practical.c
@@ -352,11 +352,11 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-int main(){
-    int myAge = 43;    
-int* ptr = &myAge;  
-printf("%d\n", myAge);
-printf("%p\n", &myAge);
-printf("%p\n", ptr);
-}
+// #include <stdio.h>
+// int main(){
+//     int myAge = 43;    
+// int* ptr = &myAge;  
+// printf("%d\n", myAge);
+// printf("%p\n", &myAge);
+// printf("%p\n", ptr);
+// }
