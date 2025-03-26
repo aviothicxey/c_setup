b455784636582a0c64c613a70c0298d54eae239a
Aparna <gargaparna78@gmail.com>
2025-03-26 17:10:18 +0530

    init

--- class.c
+++ class.c
@@ -314,3 +314,85 @@
 //     return 0;
 // }       
 
+// #include<stdio.h>
+// void merge(int arr[],int left,int mid,int right){
+//     int i,j,k;
+//     int n1=mid-left+1;
+//     int n2=right-mid;
+//     int L[n1],R[n2];
+//     for(i=0;i<n1;i++){
+//         L[i]=arr[left+i];
+//     }
+//     for(j=0;j<n2;j++){
+//         R[j]=arr[mid+1+j];
+//     }
+//     i=0;
+//     j=0;
+//     k=left;
+//     while(i<n1 && j<n2){
+//         if(L[i]<=R[j]){
+//             arr[k++]=L[i++];
+//         }
+//         else{
+//             arr[k++]=R[j++];
+//         }
+//         while(i<n1){
+//             arr[k++]=L[i++];
+//         }
+//         while(j<n2){
+//             arr[k++]=R[j++];
+//         }
+//     }
+// }
+// void mergesort(int arr[],int left,int right){
+//     if(left<right){
+//         int mid=left+(right-left)/2;
+//         mergesort(arr,left,mid);
+//         mergesort(arr,mid+1,right);
+//         merge(arr,left,mid,right);
+//     }
+// }
+// int main(){
+//     int arr[]={38,27,43,3,9,82,10};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     mergesort(arr,0,n-1);
+//     printf("Sorted array: \n");
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ",arr[i]);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int linearsearch(int arr[],int n,int x){
+//     for (int i = 0; i < n; i++)
+//     {
+//         if(arr[i]==x){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+// int main(){
+//     int n , key;
+//     printf("Enter the number of elements: ");
+//     scanf("%d",&n);
+//     int arr[n];
+//     printf("Enter %d elements: \n",n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+//     printf("Enter the element to be searched: ");
+//     scanf("%d",&key);
+//     int result = linearsearch(arr,n,key);
+//     if(result==-1){
+//         printf("Element not found\n");
+//     }
+//     else{
+//         printf("Element found at index %d\n",result);
+//     }
+//     return 0;
+// }
+
